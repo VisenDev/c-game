28b382c8408e5cbe478c8b4e0108a2b22a2c84e4
Robert Burnett <dr.slate2@gmail.com>
2026-03-19 17:02:27 -0500

    wrote serialization functions

--- stage1.c
+++ stage1.c
@@ -3,76 +3,96 @@
 #define CORE_IMPLEMENTATION
 #include "core.h"
 
+void print_type(Sexpr * type, Bool readably) {
+    if(type->tag == S_CONS) {
+        if(streql(s_first(type)->sym.v, "array")) {
+            if(readably) {
+                print_type(s_second(type), readably);
+                printf("_array");
+            } else {
+                printf("core_Vec(");
+                print_type(s_second(type), readably);
+                printf(")");
+            }
+        } else {
+            FATAL_ERROR("Unknown compound type");
+        }
+    } else if(type->tag == S_SYM) {
+        const char * s = type->sym.v;
+        if(streql(s, "string")) {
+            if(readably) {
+                printf("string");
+            } else {
+                printf("const char * ");
+            } 
+        } else {
+            printf("%s", type->sym.v);
+        }
+    } else UNREACHABLE;
+}
+
 void handle_field_definition(Sexpr * v, int i, void * ctx) {
-    Sexpr * t = s_first(v);
-    Sexpr * n = s_second(v);
-    const char * type = t->sym.v;
-    const char * name = n->sym.v;
-    size_t len = strlen(type);
-    Bool array = CORE_FALSE;
-    char buf[1024];
-    memcpy(buf, type, MIN(len + 1, sizeof(buf)));
-    buf[sizeof(buf) - 1] = 0;
-
-    assert(t->tag == S_SYM);
-    assert(n->tag == S_SYM);
-
-    if(len > 2 && type[len - 2] =='[' && type[len - 1] == ']') {
-        Bool array = true;
-        buf[len - 2] = 0;
-    }
-
-    Sexpr string = s_sym("string");
     printf("    ");
-
-    if(array) {
-        printf("core_Vec(");
-    }
-    
-    if(s_equal(s_first(v), &string)) {
-        printf("const char *");
-    } else {
-        printf("%s", buf);
-    }
-
-    if(array) {
-        printf(")");
-    }
+    print_type(s_first(v), CORE_FALSE);
     printf(" ");
     s_print(s_second(v));
     printf(";\n");
 }
 
-void generate_serialize_field_statement(Sexpr * field, int i, void * ctx) {
-    Sexpr * t = s_first(field);
-    Sexpr * n = s_second(field);
-    const char * type = t->sym.v;
-    const char * name = n->sym.v;
-    assert(t->tag == S_SYM);
-    assert(n->tag == S_SYM);
+void generate_serialize_value_statement(Sexpr * stype, char * accessor) {
+    if(stype->tag == S_SYM) {
+        const char * type = stype->sym.v;
 
-    printf("    /*%s*/\n", name);
-    printf("    fprintf(stream, \" :%s \");\n", name);
-    if(streql(type, "float")) {
-        printf("    fprintf(stream, \"%%f\", value->%s);\n", name);
-    } else if(streql(type, "int")) {
-        printf("    fprintf(stream, \"%%d\", value->%s);\n", name);
-    } else if(streql(type, "long")) {
-        printf("    fprintf(stream, \"%%ld\", value->%s);\n", name);
-    } else if(streql(type, "string")) {
-        printf("    fprintf(stream, \"\\\"%%s\\\"\", value->%s);\n", name);
+        if(streql(type, "float")) {
+            printf("    fprintf(stream, \"%%f\", %s);\n", accessor);
+        } else if(streql(type, "int")) {
+            printf("    fprintf(stream, \"%%d\", %s);\n", accessor);
+        } else if(streql(type, "long")) {
+            printf("    fprintf(stream, \"%%ld\", %s);\n", accessor);
+        } else if(streql(type, "string")) {
+            printf("    fprintf(stream, \"\\\"%%s\\\"\", %s);\n", accessor);
+        } else {
+            printf("    serialize_");
+            print_type(stype, CORE_TRUE);
+            printf("(stream, &%s);\n", accessor);
+        }
     } else {
-        printf("    serialize_%s(stream, &value->%s);\n", type, name);
+        printf("    serialize_");
+        print_type(stype, CORE_TRUE);
+        printf("(stream, %s.items, %s.len);\n", accessor, accessor);
     }
 }
 
-void generate_serialize_function(const char * name, Sexpr * fields) {
+
+void generate_serialize_field_statement(Sexpr * field, int i, void * ctx) {
+    Sexpr * stype = s_first(field);
+    const char * name = s_second(field)->sym.v;
+    char buf[1024];
+    core_snprintf(buf, sizeof(buf), "value->%s", name);
+    /* printf("    /\*%s*\/\n", name); */
+    printf("    fprintf(stream, \" :%s \");\n", name);
+    generate_serialize_value_statement(stype, buf);
+} 
+
+void generate_serialize_function(Sexpr * sname, Sexpr * fields) {
+    const char * name = sname->str.v;
     printf("void serialize_%s(FILE * stream, %s * value) {\n", name, name);
     printf("    if(!stream || !value) return;\n");
-    printf("    fprintf(stream, \"(%s\");\n", name);
+    printf("    fprintf(stream, \" #S(%s\");\n", name);
     s_do_list(fields, generate_serialize_field_statement, NULL);
     printf("    fprintf(stream, \")\");\n");
     printf("}\n\n");
+
+    printf("void serialize_%s_array(FILE * stream, %s * items, size_t n) {\n", name, name);
+    printf("    size_t i;\n");
+    printf("    if(!stream || !items) return;\n");
+    printf("    fprintf(stream, \"(list \");\n");
+    printf("    for(i = 0; i < n; ++i) {\n");
+    generate_serialize_value_statement(sname, "items[i]");
+    printf("    }");
+    printf("    fprintf(stream, \")\");\n");
+    printf("}\n\n");
+
 }
 
 void handle_defstruct(Sexpr * v) {
@@ -81,7 +101,7 @@ void handle_defstruct(Sexpr * v) {
     s_do_list(s_cdr(v), handle_field_definition, NULL);
     printf("} %s;\n\n", name);
 
-    generate_serialize_function(name, s_cdr(v));
+    generate_serialize_function(s_car(v), s_cdr(v));
 }
 
 void handle_toplevel_form(Sexpr * v, int i, void * ctx) {
@@ -99,6 +119,7 @@ int main() {
     Arena a = {0};
     Sexpr * s = s_read(&a, "structs.sexpr");
     printf("#include <stdio.h>\n");
+    printf("#include \"core.h\"\n");
     s_do_list(s, handle_toplevel_form, NULL);
     arena_free(&a);
     return 0;
--- test.c
+++ test.c
@@ -1,8 +1,18 @@
+#define CORE_STRIP_PREFIX
+#define CORE_IMPLEMENTATION
+#include "core.h"
 #include "generated.c"
 
 int main() {
     entity foo = {0};
+    Arena a = {0};
+    vec2 pos = {1.0, 2.0};
     foo.name = "bar";
+    foo.health = 100;
+    vec_append(&foo.path, &a, pos);
+    vec_append(&foo.path, &a, pos);
+    vec_append(&foo.path, &a, pos);
+    vec_append(&foo.path, &a, pos);
     serialize_entity(stdout, &foo);
     printf("\n");
     return 0;
